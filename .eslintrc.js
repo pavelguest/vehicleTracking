1693309e30bb5a5d24dd79123a20de5bf3bf8cf0
pavelguest <pavelguest@yandex.ru>
2023-07-13 16:11:53 +0300

    feat: add splashscreen and app icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'prettier/prettier': ['error', {bracketSpacing: true}],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'require-await': ['error'],
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-console': ['warn', {allow: ['warn', 'error']}],
+        'react/no-unstable-nested-components': ['warn', {allowAsProps: true}],
+      },
+    },
+  ],
 };
